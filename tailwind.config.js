8781216ce430b9dc0667f413ffae691f38ba6205
Miguel Angel <miguel.delta21@gmail.com>
2024-09-20 21:02:19 -0500

    Creación Página Adop-Design

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    colors: {
+      'carmesi' : '#9c023e',
+    },
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)'},
+          '50%': { transform: 'rotate(3deg)'},
+          }
+        },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      }
+      }
+    },
+  plugins: [],
+  }
