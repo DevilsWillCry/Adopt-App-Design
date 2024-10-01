54abbc15c8638026209d72f092218a5673ac5eb7
Miguel Angel <miguel.delta21@gmail.com>
2024-10-01 14:31:27 -0500

    json-server render

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const jsonServer = require("json-server");
+import jsonServer from "json-server";
 const server = jsonServer.create();
-const router = jsonServer.router("./src/api/db.json")
+const router = jsonServer.router("./src/api/db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
@@ -10,4 +10,3 @@ server.use(router);
 server.listen(port, () => {
     console.log(`JSON Server is running on port ${port}`);
 });
-
