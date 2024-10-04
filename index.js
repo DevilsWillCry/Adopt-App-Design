4cc2d2a5d1b75f1ac5bf921a657230f4d29a793a
Miguel Angel <miguel.delta21@gmail.com>
2024-10-04 18:07:03 -0500

    json-server render

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const jsonServer = require("json-server");
 const path = require('path');
+const express = require('express'); // Asegúrate de tener express instalado
 const server = jsonServer.create();
 const router = jsonServer.router("./src/api/db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+// Usa middlewares
 server.use(middlewares);
+
+// Configura el json-server
 server.use(router);
 
-// Servir una página HTML específica al acceder a la raíz
-server.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/pages/', 'index.html')); 
-    });
-
+// Inicia el servidor
 server.listen(port, () => {
     console.log(`JSON Server is running on port ${port}`);
 });
