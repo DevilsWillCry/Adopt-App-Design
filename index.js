909300b5721571a014f3a2aab0de54b143f1b2cf
Miguel Angel <miguel.delta21@gmail.com>
2024-10-04 14:44:52 -0500

    deploy app vercel

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ server.use(router);
 
 // Servir una página HTML específica al acceder a la raíz
 server.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/pages', 'index.html')); 
+    res.sendFile(path.join(__dirname, 'src/pages/', 'index.html')); 
     });
 
 server.listen(port, () => {
